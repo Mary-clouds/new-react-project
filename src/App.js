4b4488f001817e6ec5f91878b24bd68045dae0f9
Mary <marynevermann@gmail.com>
2025-01-20 17:20:09 +0100

    added github repository

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+     <h1> Weather App</h1>
+      <footer>
+        This React App was coded by Nevermann and
+        is opensourced in <a href='https://github.com/Mary-clouds/new-react-project' target='blank'>
+          GitHub
+        </a>
+      </footer>
     </div>
   );
 }
