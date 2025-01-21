824bc84f8f9523a23ff7a1d4e99ac6e1742d4d34
Mary <marynevermann@gmail.com>
2025-01-21 10:24:35 +0100

    added the structure of the weather app with fake data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Weather from  './weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
-     <h1> Weather App</h1>
+     <Weather/>
       <footer>
         This React App was coded by Nevermann and
         is opensourced in <a href='https://github.com/Mary-clouds/new-react-project' target='blank'>
