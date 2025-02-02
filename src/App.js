195fa361ed2e1e0a6e333f0316a9b32a843eb4da
Mary <marynevermann@gmail.com>
2025-02-02 11:30:01 +0100

    add a link for netlify

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         This React App was coded by Nevermann and
         is opensourced in <a href='https://github.com/Mary-clouds/new-react-project' target='blank' rel='noopener noreferrer'>
           GitHub
-        </a> and hosted on <a href='' target='blank' rel='noopener noreferrer'> Netlify</a>
+        </a> and hosted on <a href='https://github.com/Mary-clouds/new-react-project' target='blank' rel='noopener noreferrer'> Netlify</a>
       </footer>
       </div>
     </div>
